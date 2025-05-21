e23c0cdcb971517234e3b1abc0081b10bb76d5ca
khalifa-pendrops <dikee5200@gmail.com>
2025-05-21 13:44:34 +0100

    Remove dist from tracking

--- dist/routes/account.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("route file hit!");
-const express_1 = require("express");
-const account_controller_1 = require("../controllers/account.controller");
-const router = (0, express_1.Router)();
-router.post("/create", account_controller_1.createAccountHandler);
-router.get("/get-all", account_controller_1.getAllAccountsHandler);
-exports.default = router;
--- dist/routes/encryption.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const encryption_controller_1 = require("../controllers/encryption.controller");
-const router = (0, express_1.Router)();
-router.post("/decrypt", encryption_controller_1.decryptDataHandler);
-exports.default = router;
